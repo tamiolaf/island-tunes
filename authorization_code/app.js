aa31ddc4ede53d0623f7971c09ebfc437d412016
Tami Olafunmiloye <tamiolaf@gmail.com>
2020-11-29 02:10:59 -0500

    pixelate image, hide env variables

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -7,14 +7,15 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
+require('dotenv').config({ path: '../.env' });
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
